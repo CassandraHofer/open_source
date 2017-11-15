32623c6c44b95d2d698e6437ed49648e6718c2b6
hoferc <hoferc@pc5403p.lan.esiee.fr>
2017-11-15 10:30:04 +0100

    Classe Histogramme terminée

--- histogramme.py
+++ histogramme.py
@@ -5,43 +5,28 @@ Created on Wed Nov  8 10:45:30 2017
 """
 
 import numpy as np # import package
-import matplotlib.pyplot as plt # import module
-
-x = np.random.randn(10000)
-y = np.random.randn(10000)
-a = np.random.randn(10000)
-b = np.random.randn(10000)
-data = np.vstack([x, y, a, b]).T
-
-b = list(range(-4,5,1))
-
-n, bins, patches = plt.hist(data, bins=b,label=['x','y','a','b'], color=['crimson', 'burlywood', 'darkmagenta', 'olive'])
-
-plt.xlabel('bins')
-plt.ylabel('count')
-plt.legend(loc='upper right')
-plt.title('10.000 random numbers')
-
-plt.show()
+import matplotlib.pyplot as plt
 
 class Histogramme:
-    def __init__(self, x, y= null, a= null, b= null, xlabel, ylabel, tittle):
+    def __init__(self, w, x, y, z, b, xlabel, ylabel, tittle):
         #Faire de x, y, etc des dico contenant label et la liste de valeurs
+        self.w = w
         self.x = x
         self.y = y
-        self.a = a
+        self.z = z
         self.b = b
         self.xlabel = xlabel
         self.ylabel = ylabel
         self.tittle = tittle
-    def creerHisto(self):
-        if b != null :
-            data = np.vstack([x, y, a, b]).T
-            label = ['x','y','a','b']                
-        elif a != null:
-            data = np.vstack([x, y, a]).T
-            label = ['x','y','a','b']  
-        elif y != null:
-            data = np.vstack([x, y]).T
         
+    def afficherHistogramme(self):
+        data = np.vstack([self.w, self.x, self.y, self.z]).T
+                        
+        #b = ["Gare de Lyon", "Montparnasse", "La Rochelle"];
+        n, bins, patches = plt.hist(data, bins=self.b, label= ['2013','2014','2015','2016'], color= ['crimson', 'burlywood', 'darkmagenta', 'olive'])
         
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.legend(loc='upper right')
+        plt.title(self.tittle)
+        plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  8 10:13:15 2017
+Created on Wed Nov 15 09:06:13 2017
 
-@author: lanciar
+@author: hoferc
 """
-import histogramme.py
+import numpy as np
+
+import histogramme as histo
+
+def main():
+    w = np.random.randn(10000)
+    x = np.random.randn(10000)
+    y = np.random.randn(10000)
+    z = np.random.randn(10000)
+
+    b = list(range(-4,5,1))
+
+    histogramme = histo.Histogramme(w, x, y, z, b, "bins", "count", "10.000 random numbers")
+    histogramme.afficherHistogramme()
+                                         
+
+    
+
+
+if __name__ == "__main__":
+    main()
