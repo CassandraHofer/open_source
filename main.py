23ed972e6ad0efbc631dc97360d8ea931651a6cd
Cassandra <cassandra.hofer@gmail.com>
2017-11-15 22:16:52 +0100

    Ajout du fichier Telechargement.py permettant de télécharger un fichier depuis une url

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ Created on Wed Nov 15 09:06:13 2017
 import numpy as np
 
 import histogramme as histo
+import telechargement as telg
 
 def main():
+    fichier = telg.Telechargement("https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2016/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", "D:/Bureau/E3FI/Python/2016.csv")
+    fichier.chargement()
     w = np.random.randn(10000)
     x = np.random.randn(10000)
     y = np.random.randn(10000)
@@ -18,10 +21,6 @@ def main():
 
     histogramme = histo.Histogramme(w, x, y, z, b, "bins", "count", "10.000 random numbers")
     histogramme.afficherHistogramme()
-                                         
-
-    
-
 
 if __name__ == "__main__":
     main()
