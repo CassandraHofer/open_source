de83fdc4d22d239500f7f320c30614f15f982f04
lanciar <lanciar@pc5403w.lan.esiee.fr>
2017-11-22 10:58:14 +0100

    Modif class main et extract

--- extract.py
+++ extract.py
@@ -5,58 +5,56 @@ Created on Tue Nov 21 21:58:03 2017
 @author: romain
 """
 
-import csv, json
+import csv
 from collections import namedtuple
 
-def build_dict(csvfile):
-    """
-    retourne un dictionnaire des stations de transports en commun du fichier passé en argument
 
-    Args:
-        csvfile: un fichier au format csv contenant une liste de stations de transports en commun
-
-    Returns:
-        dictionnaire de namedtuple des informations relatives aux stations de transports en commun
+class Extract:
+    
+    def __init__(self, csvfile):
+        self.csvfile = csvfile
         
-    >>> d = build_hist_dict('trafic-annuel-entrant-par-station-du-reseau-ferre-2016.csv')
-    >>> print(d['GARE DE LYON'])
-    Station(Trafic='36352115', Rang='3', Ville='Paris')
-    >>> print(d['NOISY-CHAMPS'])
-    Station(Trafic='4868790', Rang='20', Ville='Champs-sur-Marne')
-    >>> print(d['GARE DE LYON'].Trafic)
-    36352115
-    >>> print(d['NOISY-CHAMPS'].Ville)
-    Champs-sur-Marne
-    """
+        
+    def build_dict(self):
+        """
+        retourne un dictionnaire des stations de transports en commun du fichier passé en argument
     
-    # Construire le namedtuple
-    Station = namedtuple('Station', ['Trafic', 'Rang', 'Ville'])
-    # CrÃ©er un dictionnaire vide pour stocker les donnÃ©es
-    d = dict()
-    # ouvrir le fichier csv
-    # utiliser un objet DictReader pour lire le contenu du fichier (quel dÃ©limiteur ?)
-    #     pour chaque ligne
-    #         ajouter au dictionnaire une paire clÃ©-valeur
-    #         la clÃ© est construite avec une information contenue dans la ligne
-    #         la valeur est le namedtuple Station dont les champs sont contenus dans la ligne
-    with open('./2016.csv') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=';')
-        for ligne in reader:
-            d[ligne['Station']] = Station(ligne['Trafic'], ligne['Rang'], ligne['Ville'])
-            
-            
+        Args:
+            csvfile: un fichier au format csv contenant une liste de stations de transports en commun
     
-    return d
-
-if __name__ == '__main__':
-    # votre code de test ici...
-    d = build_dict('./2016.csv')
-    print(d['GARE DE LYON'])
-    print(d['NOISY-CHAMPS'])
-    print(d['GARE DE LYON'].Trafic)
-    print(d['NOISY-CHAMPS'].Rang)
-    print(d['NOISY-CHAMPS'].Ville)
-    print('Writing JSON...')
-    with open('trafic-annuel-entrant-par-station-du-reseau-ferre-2016', 'w') as jsonfile:
-        json.dump(d, jsonfile)
-    print('Done !')
+        Returns:
+            dictionnaire de namedtuple des informations relatives aux stations de transports en commun
+            
+        >>> d = build_hist_dict('trafic-annuel-entrant-par-station-du-reseau-ferre-2016.csv')
+        >>> print(d['GARE DE LYON'])
+        Station(Trafic='36352115', Rang='3', Ville='Paris')
+        >>> print(d['NOISY-CHAMPS'])
+        Station(Trafic='4868790', Rang='20', Ville='Champs-sur-Marne')
+        >>> print(d['GARE DE LYON'].Trafic)
+        36352115
+        >>> print(d['NOISY-CHAMPS'].Ville)
+        Champs-sur-Marne
+        """
+        
+        # Construire le namedtuple
+        Station = namedtuple('Station', ['Trafic', 'Rang', 'Ville'])
+        # Crée un dictionnaire vide pour stocker les données
+        d = dict()
+        # pour chaque ligne on ajoute au dictionnaire une paire clé-valeur
+        # la clé est construite avec une information contenue dans la ligne
+        # la valeur est le namedtuple Station dont les champs sont contenus dans la ligne
+        with open('./2016.csv') as self.csvfile:
+            reader = csv.DictReader(self.csvfile, delimiter=';')
+            for ligne in reader:
+                d[ligne['Station']] = Station(ligne['Trafic'], ligne['Rang'], ligne['Ville'])       
+        return d
+    
+    
+    def get_trafic(self):
+        
+        lst_trafic = []
+        stations = self.build_dict()
+        for station in stations.keys():
+            lst_trafic.append((int)(stations[station].Trafic))
+                
+        return lst_trafic
--- main.py
+++ main.py
@@ -6,23 +6,63 @@ Created on Wed Nov 15 09:06:13 2017
 """
 import numpy as np
 
-import histogramme as histo
+import histogramme_bar as histo
 import telechargement as telg
 import extract as ext
 
 def main():
-    
+    # Telechargement
     telg.Telechargement("https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2016/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", "./2016.csv")
     telg.Telechargement("https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2015/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", "./2015.csv")
     telg.Telechargement("https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2014/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", "./2014.csv")
     telg.Telechargement("https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", "./2013.csv")
-    donnees_2016 = ext.build_dict('./2016.csv')
-    donnees_2015 = ext.build_dict('./2015.csv')
-    donnees_2014 = ext.build_dict('./2014.csv')
-    donnees_2013 = ext.build_dict('./2013.csv')
+    trafic_2016 = ext.Extract('./2016.csv').get_trafic()
     
-    stations_diag_metro = ("GARE DU NORD","SAINT-LAZARE","GARE DE LYON","MONTPARNASSE-BIENVENUE","GARE DE L'EST")
-    stations_diag_rer = ("GARE DU NORD-RER","GARE DE LYON-RER","LA DEFENSE-RER","CHATELET-LES-HALLES-RER","NANTERRE-PREFECTURE")
+    
+#    donnees_2015 = ext('./2015.csv').build_dict()
+#    donnees_2014 = ext('./2014.csv').build_dict()
+#    donnees_2013 = ext('./2013.csv').build_dict()
+    
+#    stations_diag_metro = ("GARE DU NORD","SAINT-LAZARE","GARE DE LYON","MONTPARNASSE-BIENVENUE","GARE DE L'EST")
+#    stations_diag_rer = ("GARE DU NORD-RER","GARE DE LYON-RER","LA DEFENSE-RER","CHATELET-LES-HALLES-RER","NANTERRE-PREFECTURE")
+    
+#    print(donnees_2016['GARE DE LYON'])
+#    print(donnees_2016['NOISY-CHAMPS'])
+#    print(donnees_2016['GARE DE LYON'].Trafic)
+#    print(donnees_2016['NOISY-CHAMPS'].Rang)
+#    print(donnees_2016['NOISY-CHAMPS'].Ville)
+#    print('Done')
+    for e in trafic_2016:
+        print(e)
+        
+    count0_10 = 0
+    count10_20 = 0
+    count20_30 = 0
+    count30_40 = 0
+    count40_50 = 0
+    count50_60 = 0
+    for trafic in trafic_2016:
+        if trafic < 10000000:
+            count0_10 += 1
+        elif trafic < 20000000:
+            count10_20 += 1
+        elif trafic < 30000000:
+            count20_30 += 1
+        elif trafic < 40000000:
+            count30_40 += 1
+        elif trafic < 50000000:
+            count40_50 += 1
+        elif trafic < 60000000:
+            count50_60 += 1
+    print("\n")        
+    print(count0_10)
+    print(count10_20)
+    print(count20_30)
+    print(count30_40)
+    print(count40_50)
+    print(count50_60)
+        
+        
     
     w = np.random.randn(10000)
     x = np.random.randn(10000)
@@ -33,6 +73,7 @@ def main():
 
     histogramme = histo.Histogramme_Bar(w, x, y, z, b, "bins", "count", "10.000 random numbers")
     histogramme.afficherHistogramme()
+    
 
 if __name__ == "__main__":
     main()
