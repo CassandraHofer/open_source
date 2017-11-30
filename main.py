c15d94207fafadd18b2c28e7a9123ee11dea095d
NiamoRola <romain.lancia1@gmail.com>
2017-11-30 19:27:15 +0100

    Ajout de contenu (gares sur carte)

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Created on Wed Nov 15 09:06:13 2017
 
 import pip
 pip.main(['install', 'folium'])
-pip.main(['install', 'unidecode'])
 import histogramme as histo
 import telechargement as telg
 import extract as ext
@@ -40,7 +39,7 @@ def main():
     stations_rer = ["GARE DU NORD-RER","GARE DE LYON-RER","LA DEFENSE-RER","CHATELET-LES HALLES-RER","NANTERRE-PREFECTURE"]
     stations_metro = ["GARE DU NORD","SAINT-LAZARE","GARE DE LYON","MONTPARNASSE-BIENVENUE","GARE DE L'EST"]
     stations_top5 = ["GARE DU NORD", "GARE DU NORD-RER", "SAINT-LAZARE", "GARE DE LYON-RER", "GARE DE LYON"]
-    stations_map = ['Porte de Vincennes', 'Gare de Lyon', 'Tuileries', 'Grands Boulevards', 'Billancourt', 'Aubervilliers Pantin (4 Chemins)', 'Porte d\'Italie', 'Pont Neuf', 'Montparnasse-Bienvenue', 'Les Halles', 'Bercy', 'Porte de Clignancourt', 'Riquet', 'Nogent-sur-Marne', 'Gare du Nord', 'Nanterre-Préfecture']
+    stations_map = ['Porte de Vincennes', 'Gare de Lyon', 'Tuileries', 'Grands Boulevards', 'Billancourt', 'Aubervilliers Pantin (4 Chemins)', 'Porte d\'Italie', 'Pont Neuf', 'Montparnasse-Bienvenue', 'Les Halles', 'Bercy', 'Porte de Clignancourt', 'Riquet', 'Nogent-sur-Marne', 'Gare du Nord', 'Passy', 'Drancy', 'Saint-Lazare', 'Val de Fontenay', 'Place de Clichy', 'Rueil-Malmaison', 'Boulogne Pont de Saint-Cloud', 'Chatou-Croissy', 'Porte des Lilas', 'Denfert-Rochereau', 'Oberkampf', 'Porte de Saint-Ouen', 'Arcueil-Cachan', 'Joinville-le-Pont', 'Charenton-Ecoles', 'Porte de Versailles', 'Bagneux', 'Mairie de Clichy', 'Mairie de Montreuil', 'Rue des Boulets', 'Pont de Neuilly']
     trafic_rer_2016 = []
     trafic_rer_2015 = []
     trafic_rer_2014 = []
